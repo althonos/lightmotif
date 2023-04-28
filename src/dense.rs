f23e1e23da05fe417ce55354c0a69d41840e433b
Martin Larralde <martin.larralde@embl.de>
2023-04-28 17:01:44 +0200

    Reformat code with `cargo fmt`

--- src/dense.rs
+++ src/dense.rs
@@ -8,32 +8,27 @@ pub struct DenseMatrix<T: Default, const C: usize = 32> {
     indices: Vec<usize>,
 }
 
-impl <T: Default, const C: usize> DenseMatrix<T, C> {
-    /// Create a new matrix 
+impl<T: Default, const C: usize> DenseMatrix<T, C> {
+    /// Create a new matrix
     pub fn new(rows: usize) -> Self {
         // allocate data block
-        let mut data = Vec::with_capacity(rows*C + C - 1); // FIXME
+        let mut data = Vec::with_capacity(rows * C + C - 1); // FIXME
         for _ in 0..data.capacity() {
             data.push(T::default());
         }
         // compute offset to have proper alignment
-        let offset = unsafe {
-            data.align_to::<[u8; C]>().0.len()
-        };
+        let offset = unsafe { data.align_to::<[u8; C]>().0.len() };
         // record row coordinates (only really useful when aligned)
         let mut indices = Vec::with_capacity(rows);
         for i in 0..rows {
-            indices.push(offset + i*C)
+            indices.push(offset + i * C)
         }
         // finish matrix
-        Self {
-            data,
-            indices,
-        }
+        Self { data, indices }
     }
 
     /// The number of columns of the matrix.
-    pub const fn columns(&self) -> usize { 
+    pub const fn columns(&self) -> usize {
         C
     }
 
@@ -47,13 +42,13 @@ impl<T: Default, const C: usize> Index<usize> for DenseMatrix<T, C> {
     type Output = [T];
     fn index(&self, index: usize) -> &Self::Output {
         let row = self.indices[index];
-        &self.data[row..row+C]
+        &self.data[row..row + C]
     }
 }
 
 impl<T: Default, const C: usize> IndexMut<usize> for DenseMatrix<T, C> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         let row = self.indices[index];
-        &mut self.data[row..row+C]
+        &mut self.data[row..row + C]
     }
 }
