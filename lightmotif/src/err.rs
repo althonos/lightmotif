0d1484c5c59ceefd1e50c319f8ca52a05cf07e76
Martin Larralde <martin.larralde@embl.de>
2023-05-03 14:01:16 +0200

    Add `Error` trait impls to `InvalidSymbol` and `InvalidData`

--- lightmotif/src/err.rs
+++ lightmotif/src/err.rs
@@ -1,7 +1,28 @@
+use std::error::Error;
+use std::fmt::Display;
+use std::fmt::Error as FmtError;
+use std::fmt::Formatter;
+
 /// The given character is not a valid symbol.
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct InvalidSymbol(pub char);
 
+impl Display for InvalidSymbol {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), FmtError> {
+        write!(f, "invalid symbol {:?} found", self.0)
+    }
+}
+
+impl Error for InvalidSymbol {}
+
 /// Invalid data was passed to initialize the matrix.
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct InvalidData;
+
+impl Display for InvalidData {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), FmtError> {
+        f.write_str("invalid data found")
+    }
+}
+
+impl Error for InvalidData {}
