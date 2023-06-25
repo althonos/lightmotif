dad6a9e1051a80b5e893597bbb3c51850e2b3fec
Martin Larralde <martin.larralde@embl.de>
2023-06-25 20:25:27 +0200

    Add benchmarks for exact computations in `lightmotif-tfmpvalue`

---
+++ lightmotif-tfmpvalue/benches/exact.rs
@@ -0,0 +1,40 @@
+#![feature(test)]
+
+extern crate lightmotif;
+extern crate lightmotif_tfmpvalue;
+extern crate test;
+
+use lightmotif::abc::Dna;
+use lightmotif::pwm::CountMatrix;
+use lightmotif::seq::EncodedSequence;
+use lightmotif_tfmpvalue::TfmPvalue;
+
+#[bench]
+fn bench_pvalue(bencher: &mut test::Bencher) {
+    let pssm = CountMatrix::<Dna>::from_sequences(&[
+        EncodedSequence::encode("GTTGACCTTATCAAC").unwrap(),
+        EncodedSequence::encode("GTTGATCCAGTCAAC").unwrap(),
+    ])
+    .unwrap()
+    .to_freq(0.25)
+    .to_scoring(None);
+    let mut tfmp = TfmPvalue::new(&pssm);
+    bencher.iter(|| {
+        test::black_box(tfmp.pvalue(8.0));
+    });
+}
+
+#[bench]
+fn bench_score(bencher: &mut test::Bencher) {
+    let pssm = CountMatrix::<Dna>::from_sequences(&[
+        EncodedSequence::encode("GTTGACCTTATCAAC").unwrap(),
+        EncodedSequence::encode("GTTGATCCAGTCAAC").unwrap(),
+    ])
+    .unwrap()
+    .to_freq(0.25)
+    .to_scoring(None);
+    let mut tfmp = TfmPvalue::new(&pssm);
+    bencher.iter(|| {
+        test::black_box(tfmp.score(1e-3));
+    });
+}
