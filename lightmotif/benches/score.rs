3f5452508f6c6501c08e0a655a63f40bf26d70ba
Martin Larralde <martin.larralde@embl.de>
2023-07-31 16:57:11 +0200

    Add `lightmotif` benchmarks for protein PSSMs

--- lightmotif/benches/score.rs
+++ lightmotif/benches/score.rs
@@ -3,7 +3,6 @@
 extern crate lightmotif;
 extern crate test;
 
-use lightmotif::abc::Dna;
 use lightmotif::num::StrictlyPositive;
 use lightmotif::num::U16;
 use lightmotif::num::U32;
@@ -14,51 +13,116 @@ use lightmotif::pwm::CountMatrix;
 use lightmotif::seq::EncodedSequence;
 use lightmotif::seq::StripedSequence;
 
-const SEQUENCE: &str = include_str!("ecoli.txt");
+mod dna {
 
-fn bench<C: StrictlyPositive, P: Score<Dna, C>>(bencher: &mut test::Bencher, pli: &P) {
-    let mut striped = StripedSequence::<Dna, C>::encode(SEQUENCE).unwrap();
+    use super::*;
+    use lightmotif::abc::Dna;
 
-    let cm = CountMatrix::<Dna>::from_sequences(&[
-        EncodedSequence::encode("GTTGACCTTATCAAC").unwrap(),
-        EncodedSequence::encode("GTTGATCCAGTCAAC").unwrap(),
-    ])
-    .unwrap();
-    let pbm = cm.to_freq(0.1);
-    let pssm = pbm.to_scoring(None);
+    const SEQUENCE: &str = include_str!("ecoli.txt");
 
-    striped.configure(&pssm);
-    let mut scores = StripedScores::empty();
-    bencher.bytes = SEQUENCE.len() as u64;
-    bencher.iter(|| {
-        pli.score_into(&striped, &pssm, &mut scores);
-        test::black_box(())
-    });
+    fn bench<C: StrictlyPositive, P: Score<Dna, C>>(bencher: &mut test::Bencher, pli: &P) {
+        let mut striped = StripedSequence::<Dna, C>::encode(SEQUENCE).unwrap();
+
+        let cm = CountMatrix::<Dna>::from_sequences(&[
+            EncodedSequence::encode("GTTGACCTTATCAAC").unwrap(),
+            EncodedSequence::encode("GTTGATCCAGTCAAC").unwrap(),
+        ])
+        .unwrap();
+        let pbm = cm.to_freq(0.1);
+        let pssm = pbm.to_scoring(None);
+
+        striped.configure(&pssm);
+        let mut scores = StripedScores::empty();
+        bencher.bytes = SEQUENCE.len() as u64;
+        bencher.iter(|| {
+            pli.score_into(&striped, &pssm, &mut scores);
+            test::black_box(())
+        });
+    }
+
+    #[bench]
+    fn bench_generic(bencher: &mut test::Bencher) {
+        let pli = Pipeline::generic();
+        bench::<U32, _>(bencher, &pli);
+    }
+
+    #[cfg(target_feature = "sse2")]
+    #[bench]
+    fn bench_sse2(bencher: &mut test::Bencher) {
+        let pli = Pipeline::sse2().unwrap();
+        bench::<U16, _>(bencher, &pli);
+    }
+
+    #[cfg(target_feature = "sse2")]
+    #[bench]
+    fn bench_sse2_32(bencher: &mut test::Bencher) {
+        let pli = Pipeline::sse2().unwrap();
+        bench::<U32, _>(bencher, &pli);
+    }
+
+    #[cfg(target_feature = "avx2")]
+    #[bench]
+    fn bench_avx2(bencher: &mut test::Bencher) {
+        let pli = Pipeline::avx2().unwrap();
+        bench(bencher, &pli);
+    }
 }
 
-#[bench]
-fn bench_generic(bencher: &mut test::Bencher) {
-    let pli = Pipeline::generic();
-    bench::<U32, _>(bencher, &pli);
-}
+mod protein {
 
-#[cfg(target_feature = "sse2")]
-#[bench]
-fn bench_sse2(bencher: &mut test::Bencher) {
-    let pli = Pipeline::sse2().unwrap();
-    bench::<U16, _>(bencher, &pli);
-}
+    use super::*;
+    use lightmotif::abc::Protein;
 
-#[cfg(target_feature = "sse2")]
-#[bench]
-fn bench_sse2_32(bencher: &mut test::Bencher) {
-    let pli = Pipeline::sse2().unwrap();
-    bench::<U32, _>(bencher, &pli);
-}
+    const SEQUENCE: &str = include_str!("abyB1.txt");
 
-#[cfg(target_feature = "avx2")]
-#[bench]
-fn bench_avx2(bencher: &mut test::Bencher) {
-    let pli = Pipeline::avx2().unwrap();
-    bench(bencher, &pli);
+    fn bench<C: StrictlyPositive, P: Score<Protein, C>>(bencher: &mut test::Bencher, pli: &P) {
+        let mut striped = StripedSequence::<Protein, C>::encode(SEQUENCE).unwrap();
+
+        let cm = CountMatrix::<Protein>::from_sequences(&[
+            EncodedSequence::encode("SFKELGFDSLTAVELRNRLAAAT").unwrap(),
+            EncodedSequence::encode("AFKELGFDSLAAIQLRNRLLADV").unwrap(),
+            EncodedSequence::encode("PSRRLGFDSLTAVELRNQLAAST").unwrap(),
+            EncodedSequence::encode("AFREIGFDSLTAVELRNRLGAAA").unwrap(),
+            EncodedSequence::encode("SLMEEGLDSLAAVELGGTLQRDT").unwrap(),
+            EncodedSequence::encode("GFFDLGMDSLMAVELRRRIEQGV").unwrap(),
+        ])
+        .unwrap();
+        let pbm = cm.to_freq(0.1);
+        let pssm = pbm.to_scoring(None);
+
+        striped.configure(&pssm);
+        let mut scores = StripedScores::empty();
+        bencher.bytes = SEQUENCE.len() as u64;
+        bencher.iter(|| {
+            pli.score_into(&striped, &pssm, &mut scores);
+            test::black_box(())
+        });
+    }
+
+    #[bench]
+    fn bench_generic(bencher: &mut test::Bencher) {
+        let pli = Pipeline::generic();
+        bench::<U32, _>(bencher, &pli);
+    }
+
+    #[cfg(target_feature = "sse2")]
+    #[bench]
+    fn bench_sse2(bencher: &mut test::Bencher) {
+        let pli = Pipeline::sse2().unwrap();
+        bench::<U16, _>(bencher, &pli);
+    }
+
+    #[cfg(target_feature = "sse2")]
+    #[bench]
+    fn bench_sse2_32(bencher: &mut test::Bencher) {
+        let pli = Pipeline::sse2().unwrap();
+        bench::<U32, _>(bencher, &pli);
+    }
+
+    #[cfg(target_feature = "avx2")]
+    #[bench]
+    fn bench_avx2(bencher: &mut test::Bencher) {
+        let pli = Pipeline::avx2().unwrap();
+        bench(bencher, &pli);
+    }
 }
