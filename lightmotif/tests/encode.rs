c7bdb341554c106613aecdc745b1c7ffe79014f4
Martin Larralde <martin.larralde@embl.de>
2023-08-30 22:36:13 +0200

    Add tests for the `Transpose` trait implementations

--- lightmotif/tests/encode.rs
+++ lightmotif/tests/encode.rs
@@ -10,7 +10,6 @@ use lightmotif::pli::Pipeline;
 const SEQUENCE: &str = "ATGTCCCAACAACGATACCCCGAGCCCATCGCCGTCATCGGCTCGGCATGCAGATTCCCAGGCG";
 const UNKNOWNS: &str = "ATGTCCCAACAACGATACCNN..................NNNNNNNNATGCAGATTCCCAGGCG";
 
-// scores computed with Bio.motifs
 #[rustfmt::skip]
 const EXPECTED: &[Nucleotide] = &[
     A, T, G, T, C, C, C, A, A, C, A, A, C, G, A, T, A, C, C, C, C, G, A, G, C, C, C, A, T, C, G, C, C, G, T, C, A, T, C, G, G, C, T, C, G, G, C, A, T, G, C, A, G, A, T, T, C, C, C, A, G, G, C, G 
---
+++ lightmotif/tests/stripe.rs
@@ -0,0 +1,78 @@
+extern crate lightmotif;
+extern crate typenum;
+
+use lightmotif::abc::Dna;
+use lightmotif::abc::Nucleotide;
+use lightmotif::num::NonZero;
+use lightmotif::num::Unsigned;
+use lightmotif::num::U16;
+use lightmotif::num::U32;
+use lightmotif::pli::Pipeline;
+use lightmotif::pli::Stripe;
+use lightmotif::seq::EncodedSequence;
+
+const S1: &str = "ATGTCCCAACAACGATACCCCGAGCCCATCGCCGTCATCGGCTCGGCATGCAGATTCCCAGGCGATGTCCCAACAACGATACCCCGAGCCCATCGCCGTCATCGGCTCGGCATGCAGATTCCCAGGCGATGTCCCAACAACGATACCCCGAGCCCATCGCCGTCATCGGCTCGGCATGCAGATTCCCAGGCGATGTCCCAACAACGATACCCCGAGCCCATCGCCGTCATCGGCTCGGCATGCAGATTCCCAGGCGATGTCCCAACAACGATACCCCGAGCCCATCGCCGTCATCGGCTCGGCATGCAGATTCCCAGGCGATGTCCCAACAACGATACCCCGAGCCCATCGCCGTCATCGGCTCGGCATGCAGATTCCCAGGCGATGTCCCAACAACGATACCCCGAGCCCATCGCCGTCATCGGCTCGGCATGCAGATTCCCAGGCGATGTCCCAACAACGATACCCCGAGCCCATCGCCGTCATCGGCTCGGCATGCAGATTCCCAGGCGATGTCCCAACAACGATACCCCGAGCCCATCGCCGTCATCGGCTCGGCATGCAGATTCCCAGGCGATGTCCCAACAACGATACCCCGAGCCCATCGCCGTCATCGGCTCGGCATGCAGATTCCCAGGCGATGTCCCAACAACGATACCCCGAGCCCATCGCCGTCATCGGCTCGGCATGCAGATTCCCAGGCGATGTCCCAACAACGATACCCCGAGCCCATCGCCGTCATCGGCTCGGCATGCAGATTCCCAGGCGATGTCCCAACAACGATACCCCGAGCCCATCGCCGTCATCGGCTCGGCATGCAGATTCCCAGGCGATGTCCCAACAACGATACCCCGAGCCCATCGCCGTCATCGGCTCGGCATGCAGATTCCCAGGCGATGTCCCAACAACGATACCCCGAGCCCATCGCCGTCATCGGCTCGGCATGCAGATTCCCAGGCGATGTCCCAACAACGATACCCCGAGCCCATCGCCGTCATCGGCTCGGCATGCAGATTCCCAGGCGTTATTAT";
+const S2: &str = "ATGTCCCAACAACGATACCCCGAGCCCATCGCCGTCATCGGCTCGGCATGCAGATTCCCAGGCG";
+
+fn test_stripe<C: Unsigned + NonZero, P: Stripe<Dna, C>>(pli: &P, sequence: &str) {
+    let encoded = EncodedSequence::<Dna>::encode(sequence).unwrap();
+    let striped = pli.stripe(&encoded);
+
+    if striped.data.rows() > 0 {
+        assert_eq!(striped.data[0][0], Nucleotide::A);
+    }
+    if striped.data.rows() > 1 {
+        assert_eq!(striped.data[1][0], Nucleotide::T);
+    }
+    if striped.data.rows() > 2 {
+        assert_eq!(striped.data[2][0], Nucleotide::G);
+    }
+    if striped.data.rows() > 3 {
+        assert_eq!(striped.data[3][0], Nucleotide::T);
+    }
+
+    for (i, &c) in encoded.iter().into_iter().enumerate() {
+        assert_eq!(
+            striped.data[i % striped.data.rows()][i / striped.data.rows()],
+            c
+        );
+    }
+
+    for i in sequence.len()..striped.data.rows() * striped.data.columns() {
+        assert_eq!(
+            striped.data[i % striped.data.rows()][i / striped.data.rows()],
+            Nucleotide::default(),
+        )
+    }
+}
+
+#[test]
+fn test_stripe_generic() {
+    let pli = Pipeline::generic();
+    test_stripe::<U32, _>(&pli, S1);
+    test_stripe::<U32, _>(&pli, S2);
+    test_stripe::<U16, _>(&pli, S1);
+    test_stripe::<U16, _>(&pli, S2);
+}
+
+// #[cfg(target_feature = "sse2")]
+// #[test]
+// fn test_stripe_sse2() {
+//     let pli = Pipeline::sse2().unwrap();
+//     test_stripe(&pli);
+// }
+
+// #[cfg(target_feature = "avx2")]
+// #[test]
+// fn test_stripe_avx2() {
+//     let pli = Pipeline::avx2().unwrap();
+//     test_stripe(&pli, S1);
+//     test_stripe(&pli, S2);
+// }
+
+// #[cfg(target_feature = "neon")]
+// #[test]
+// fn test_stripe_neon() {
+//     let pli = Pipeline::neon().unwrap();
+//     test_stripe(&pli);
+// }
