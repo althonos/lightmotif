751c9ac81ec54fb823ab4ba93a43efb8b9561c8e
Martin Larralde <martin.larralde@embl.de>
2024-06-14 01:11:04 +0200

    Add benchmark for striping an encoded sequence

---
+++ lightmotif/benches/stripe.rs
@@ -0,0 +1,113 @@
+#![feature(test)]
+
+extern crate lightmotif;
+extern crate test;
+
+use lightmotif::num::U32;
+use lightmotif::pli::Pipeline;
+use lightmotif::pli::Stripe;
+use lightmotif::seq::EncodedSequence;
+
+mod dna {
+
+    use super::*;
+    use lightmotif::abc::Dna;
+
+    const SEQUENCE: &str = include_str!("ecoli.txt");
+
+    fn bench<P: Stripe<Dna, U32>>(bencher: &mut test::Bencher, pli: &P) {
+        let seq = EncodedSequence::encode(SEQUENCE).unwrap();
+        let mut dst = seq.to_striped();
+
+        bencher.iter(|| {
+            pli.stripe_into(&seq, &mut dst);
+            test::black_box(());
+        });
+        bencher.bytes = SEQUENCE.as_bytes().len() as u64;
+    }
+
+    #[bench]
+    fn bench_generic(bencher: &mut test::Bencher) {
+        let pli = Pipeline::generic();
+        bench(bencher, &pli);
+    }
+
+    #[bench]
+    fn bench_dispatch(bencher: &mut test::Bencher) {
+        let pli = Pipeline::dispatch();
+        bench(bencher, &pli);
+    }
+
+    // #[cfg(target_feature = "sse2")]
+    // #[bench]
+    // fn bench_sse2(bencher: &mut test::Bencher) {
+    //     let pli = Pipeline::sse2().unwrap();
+    //     bench(bencher, &pli);
+    // }
+
+    #[cfg(target_feature = "avx2")]
+    #[bench]
+    fn bench_avx2(bencher: &mut test::Bencher) {
+        let pli = Pipeline::avx2().unwrap();
+        bench(bencher, &pli);
+    }
+
+    // #[cfg(target_feature = "neon")]
+    // #[bench]
+    // fn bench_neon(bencher: &mut test::Bencher) {
+    //     let pli = Pipeline::neon().unwrap();
+    //     bench(bencher, &pli);
+    // }
+}
+
+mod protein {
+
+    use super::*;
+    use lightmotif::abc::Protein;
+
+    const SEQUENCE: &str = include_str!("abyB1.txt");
+
+    fn bench<P: Stripe<Protein, U32>>(bencher: &mut test::Bencher, pli: &P) {
+        let seq = EncodedSequence::encode(SEQUENCE).unwrap();
+        let mut dst = seq.to_striped();
+
+        bencher.iter(|| {
+            pli.stripe_into(&seq, &mut dst);
+            test::black_box(());
+        });
+        bencher.bytes = SEQUENCE.as_bytes().len() as u64;
+    }
+
+    #[bench]
+    fn bench_generic(bencher: &mut test::Bencher) {
+        let pli = Pipeline::generic();
+        bench(bencher, &pli);
+    }
+
+    #[bench]
+    fn bench_dispatch(bencher: &mut test::Bencher) {
+        let pli = Pipeline::dispatch();
+        bench(bencher, &pli);
+    }
+
+    // #[cfg(target_feature = "sse2")]
+    // #[bench]
+    // fn bench_sse2(bencher: &mut test::Bencher) {
+    //     let pli = Pipeline::sse2().unwrap();
+    //     bench(bencher, &pli);
+    // }
+
+    #[cfg(target_feature = "avx2")]
+    #[bench]
+    fn bench_avx2(bencher: &mut test::Bencher) {
+        let pli = Pipeline::avx2().unwrap();
+        bench(bencher, &pli);
+    }
+
+    // #[cfg(target_feature = "neon")]
+    // #[bench]
+    // fn bench_neon(bencher: &mut test::Bencher) {
+    //     let pli = Pipeline::neon().unwrap();
+    //     bench(bencher, &pli);
+    // }
+}
