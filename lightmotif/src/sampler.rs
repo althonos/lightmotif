d5ba0a4cea2e9fa271c718a17fc1677e01ac2f6f
Martin Larralde <martin.larralde@embl.de>
2024-10-30 01:19:38 +0100

    Rewrite sampler to avoid counting from scratch on every iteration

--- lightmotif/src/sampler.rs
+++ lightmotif/src/sampler.rs
@@ -18,6 +18,7 @@ use super::dense::DenseMatrix;
 use super::dense::MatrixCoordinates;
 use super::num::ArrayLength;
 use super::num::NonZero;
+use super::num::Unsigned;
 use super::num::U32;
 use super::pli::dispatch::Dispatch;
 use super::pli::platform::Backend;
@@ -31,12 +32,19 @@ use super::seq::SymbolCount;
 
 #[derive(Debug)]
 pub struct GibbsSampler<A: Alphabet, C: ArrayLength + NonZero> {
+    counts: Vec<GenericArray<usize, A::K>>,
     sequences: RwLock<Vec<StripedSequence<A, C>>>,
 }
 
 impl<A: Alphabet, C: ArrayLength + NonZero> GibbsSampler<A, C> {
     pub fn new(sequences: Vec<StripedSequence<A, C>>) -> Self {
+        // count background positions
+        let counts = sequences
+            .iter()
+            .map(|seq| SymbolCount::<A>::count_symbols(seq))
+            .collect();
         Self {
+            counts,
             sequences: RwLock::new(sequences),
         }
     }
@@ -54,6 +62,14 @@ pub struct GibbsGenerator<'a, R: Rng, A: Alphabet, C: ArrayLength + NonZero> {
     width: usize,
     indices: Vec<usize>,
     temperature: f64,
+    /// The current count matrix for the motif
+    motif: DenseMatrix<u32, A::K>,
+    background_counts: GenericArray<usize, A::K>,
+}
+
+enum Op {
+    Add,
+    Sub,
 }
 
 impl<'a, R: Rng, A: Alphabet, C: ArrayLength + NonZero> GibbsGenerator<'a, R, A, C> {
@@ -81,74 +97,149 @@ impl<'a, R: Rng, A: Alphabet, C: ArrayLength + NonZero> GibbsGenerator<'a, R, A,
             .iter()
             .map(|seq| rng.sample(Uniform::new(0, seq.len() - width + 1)))
             .collect::<Vec<usize>>();
+        let mut starts = GenericArray::<usize, A::K>::default();
+        for (seq, &start) in sampler.sequences.read().unwrap().iter().zip(&indices) {
+            starts[seq[start].as_index()] += 1;
+        }
+        // build motif count with all sequences
+        let mut motif = DenseMatrix::new(width);
+        for (seq, &start) in sampler.sequences.read().unwrap().iter().zip(&indices) {
+            // compute motif counts
+            for (i, j) in (start..start + width).enumerate() {
+                motif[MatrixCoordinates::new(i, seq[j].as_index())] += 1;
+            }
+        }
+        // build background counts with all sequences
+        let mut background_counts = GenericArray::default();
+        for ((seq, &start), counts) in sampler
+            .sequences
+            .read()
+            .unwrap()
+            .iter()
+            .zip(&indices)
+            .zip(&sampler.counts)
+        {
+            for symbol in 0..A::K::USIZE {
+                background_counts[symbol] += counts[symbol];
+            }
+            for j in start..start + width {
+                background_counts[seq[j].as_index()] -= 1;
+            }
+        }
         // finish initializing the generator
         Self {
             rng,
             sampler,
             width,
             indices,
-            temperature: 2.0,
+            temperature: 1.0,
+            motif,
+            background_counts,
             pli: Pipeline::dispatch(),
+        }
+    }
+
+    fn select_holdout(&mut self) -> usize {
+        self.rng.sample(Uniform::new(0, self.indices.len()))
+    }
+
+    fn recount_motifs(&mut self, z: usize, op: Op) {
+        let sequences = self.sampler.sequences.read().unwrap();
+        let seq = &sequences[z];
+        let start = self.indices[z];
+        for (i, j) in (start..start + self.width).enumerate() {
+            match op {
+                Op::Add => self.motif[MatrixCoordinates::new(i, seq[j].as_index())] += 1,
+                Op::Sub => self.motif[MatrixCoordinates::new(i, seq[j].as_index())] -= 1,
+            }
+        }
+    }
+
+    fn recount_background(&mut self, z: usize, op: Op) -> Background<A> {
+        let counts = &self.sampler.counts[z];
+        let sequences = self.sampler.sequences.read().unwrap();
+        let seq = &sequences[z];
+        let start = self.indices[z];
+        match op {
+            Op::Sub => {
+                for j in start..start + self.width {
+                    self.background_counts[seq[j].as_index()] += 1;
+                }
+                for symbol in 0..A::K::USIZE {
+                    self.background_counts[symbol] -= counts[symbol];
+                }
+            }
+            Op::Add => {
+                for symbol in 0..A::K::USIZE {
+                    self.background_counts[symbol] += counts[symbol];
+                }
+                for j in start..start + self.width {
+                    self.background_counts[seq[j].as_index()] -= 1;
+                }
+            }
+        }
+        Background::<A>::from_counts(&self.background_counts).unwrap()
+    }
+
+    fn prepare_pssm(
+        &self,
+        motif: DenseMatrix<u32, A::K>,
+        background: Background<A>,
+    ) -> (CountMatrix<A>, ScoringMatrix<A>) {
+        let counts = CountMatrix::new(motif).unwrap();
+        let pssm = counts.to_freq(0.1).to_scoring(background);
+        (counts, pssm)
+    }
+}
+
+impl<'a, R: Rng, A: Alphabet, C: ArrayLength + NonZero> GibbsGenerator<'a, R, A, C>
+where
+    Pipeline<A, Dispatch>: Score<f32, A, C>,
+{
+    fn update_holdout(&mut self, z: usize, pssm: &ScoringMatrix<A>) {
+        let mut scores = StripedScores::empty();
+        self.pli.score_into(
+            &pssm,
+            &self.sampler.sequences.read().unwrap()[z],
+            &mut scores,
+        );
+        let weights = scores.iter().map(|&x| (x as f64 / self.temperature).exp());
+        if let Ok(dist) = WeightedIndex::new(weights) {
+            self.indices[z] = dist.sample(&mut self.rng);
         }
     }
 }
 
 impl<'a, R: Rng, A: Alphabet, C: ArrayLength + NonZero> Iterator for GibbsGenerator<'a, R, A, C>
 where
-    Pipeline<A, Dispatch>: Score<f32, A, C> + Maximum<f32, C>,
+    Pipeline<A, Dispatch>: Score<f32, A, C>,
 {
     type Item = GibbsIteration<A>;
     fn next(&mut self) -> Option<Self::Item> {
-        // buffer the motifs
-        let mut motif_counts = DenseMatrix::new(self.width);
-        let mut background_counts = GenericArray::default();
-        let mut scores = StripedScores::empty();
-
         // step 1: sampling
         // select the holdout sequence
-        let z = self.rng.sample(Uniform::new(0, self.indices.len()));
+        let z = self.select_holdout();
+        // remove holdout sequence from motif counts
+        self.recount_motifs(z, Op::Sub);
+        self.recount_background(z, Op::Sub);
         // build background & count matrix excluding sequence z
-        for (seq, &start) in self
-            .sampler
-            .sequences
-            .read()
-            .unwrap()
-            .iter()
-            .zip(&self.indices)
-            .enumerate()
-            .filter(|(i, _)| *i != z)
-            .map(|(_, x)| x)
-        {
-            // compute background counts
-            for symbol in <A as Alphabet>::symbols() {
-                background_counts[symbol.as_index()] += seq.count_symbol(*symbol);
-            }
-            for j in start..start + self.width {
-                background_counts[seq[j].as_index()] -= 1;
-            }
-            // compute motif counts
-            for (i, j) in (start..start + self.width).enumerate() {
-                motif_counts[MatrixCoordinates::new(i, seq[j].as_index())] += 1;
-            }
-        }
-        let background = Background::<A>::from_counts(&background_counts).unwrap();
-        let counts = CountMatrix::<A>::new(motif_counts).unwrap();
+        let background = Background::from_counts(&self.background_counts).unwrap();
 
         // step 2: update
-        // compute scores using PSSM
-        let pssm = counts.to_freq(0.1).to_scoring(background);
-        self.pli.score_into(
-            &pssm,
-            &self.sampler.sequences.read().unwrap()[z],
-            &mut scores,
-        );
-        // sample new position using scores as sampling weights
-        let weights = scores.iter().map(|&x| (x as f64 / self.temperature).exp());
-        let dist = WeightedIndex::new(weights).unwrap();
-        self.indices[z] = dist.sample(&mut self.rng);
+        // create PSSM from motif
+        let (cm, pssm) = self.prepare_pssm(self.motif.clone(), background);
+        // select new start position for sequence Z
+        self.update_holdout(z, &pssm);
+        // add new holdout sequence position to motif counts
+        self.recount_motifs(z, Op::Add);
+        self.recount_background(z, Op::Add);
 
         // yield current iteration
-        Some(GibbsIteration { counts, pssm, z })
+        Some(GibbsIteration {
+            pssm,
+            counts: cm,
+            z,
+        })
     }
 }
 
@@ -169,7 +260,7 @@ mod test {
 
     use rand::SeedableRng;
 
-    use crate::abc::Protein;
+    use crate::abc::{AminoAcid, Protein};
     use crate::seq::EncodedSequence;
 
     use super::*;
@@ -215,12 +306,32 @@ mod test {
             .map(EncodedSequence::<Protein>::from_str)
             .map(Result::unwrap)
             .map(StripedSequence::from)
-            .collect();
+            .collect::<Vec<_>>();
 
-        let rng = rand::rngs::StdRng::from_seed([1; 32]);
+        let encoded = sequences
+            .iter()
+            .cloned()
+            .map(EncodedSequence::<Protein>::from_str)
+            .map(Result::unwrap)
+            .collect::<Vec<_>>();
+
+        let x = &striped[3];
+        let y = &encoded[3];
+        assert_eq!(x.count_symbols(), SymbolCount::<Protein>::count_symbols(&y));
+
+        let rng = rand::rngs::mock::StepRng::new(1, 42);
         let sampler = GibbsSampler::new(striped);
 
-        let result = sampler.sample(17, rng).skip(100).next().unwrap();
-        assert!(result.counts.information_content() > 12.0);
+        let gen = sampler.sample(17, rng);
+        // println!("{:?}", gen.motif);
+        // gen.recount_motifs(0, Op::Sub);
+        // println!("{:?}", gen.motif);
+        // gen.recount_motifs(0, Op::Add);
+        // println!("{:?}", gen.motif);
+
+        let result = gen.skip(20).next().unwrap();
+        // println!("{:?}", result.pssm);
+        // panic!("ic={:?}", result.pssm.information_content());
+        assert_eq!(result.pssm.information_content(), 30.054865);
     }
 }
