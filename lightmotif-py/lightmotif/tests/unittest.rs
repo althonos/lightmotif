be2bb697e5dcb87d8cb21c9d11797d119d28837a
Martin Larralde <martin.larralde@embl.de>
2025-03-06 16:36:10 +0100

    Fix deprecated API use in `lightmotif/tests/unittest.rs`

--- lightmotif-py/lightmotif/tests/unittest.rs
+++ lightmotif-py/lightmotif/tests/unittest.rs
@@ -26,23 +26,23 @@ pub fn main() -> PyResult<()> {
     Python::with_gil(|py| {
         // insert the project folder in `sys.modules` so that
         // the main module can be imported by Python
-        let sys = py.import_bound("sys")?;
+        let sys = py.import("sys")?;
         sys.getattr("path")?
             .downcast::<PyList>()?
             .insert(0, folder)?;
 
         // create a Python module from our rust code with debug symbols
-        let module = PyModule::new_bound(py, "lightmotif.lib")?;
+        let module = PyModule::new(py, "lightmotif.lib")?;
         lightmotif_py::init(py, &module).unwrap();
         sys.getattr("modules")?
             .downcast::<PyDict>()?
             .set_item("lightmotif.lib", module)?;
 
         // run unittest on the tests
-        let kwargs = PyDict::new_bound(py);
+        let kwargs = PyDict::new(py);
         kwargs.set_item("exit", false).unwrap();
         kwargs.set_item("verbosity", 2u8).unwrap();
-        py.import_bound("unittest").unwrap().call_method(
+        py.import("unittest").unwrap().call_method(
             "TestProgram",
             ("lightmotif.tests",),
             Some(&kwargs),
